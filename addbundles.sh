1433652635d23fe24e6dbe4258564e1f748369a8
Sebastian Tramp <mail@sebastian.tramp.name>
2012-07-09 20:03:30 +0200

    fix disruptive LANG environment

--- addbundles.sh
+++ addbundles.sh
@@ -1,8 +1,10 @@
+unset LANG
 for dir in `git status -sb bundle | grep "??" | cut -d " " -f 2`;
 do
     cd $dir;
     remote=`git remote show origin | grep Fetch | cut -d " " -f 5`;
     cd ../..;
     echo $remote;
+    echo git submodule add $remote $dir;
     git submodule add $remote $dir;
 done
